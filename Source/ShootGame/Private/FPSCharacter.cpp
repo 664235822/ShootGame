f609b74c9f6c2114a79eee314ba7fed060f9679a
SmallPig1997 <664235822@qq.com>
2020-08-29 21:12:55 +0800

    人物移动蹲伏行为与动画

---
+++ Source/ShootGame/Private/FPSCharacter.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSCharacter.h"
+#include "GameFramework/PawnMovementComponent.h"
+
+// Sets default values
+AFPSCharacter::AFPSCharacter()
+{
+    // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+
+    SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SprintArmComponent"));
+    SpringArmComponent->bUsePawnControlRotation = true;
+    SpringArmComponent->SetupAttachment(RootComponent);
+
+    GetMovementComponent()->GetNavAgentPropertiesRef().bCanCrouch = true;
+    
+    CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+    CameraComponent->SetupAttachment(SpringArmComponent);
+}
+
+// Called when the game starts or when spawned
+void AFPSCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+// Called every frame
+void AFPSCharacter::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    PlayerInputComponent->BindAxis("MoveForward", this, &AFPSCharacter::MoveForward);
+    PlayerInputComponent->BindAxis("MoveRight", this, &AFPSCharacter::MoveRight);
+    PlayerInputComponent->BindAxis("LookUp", this, &AFPSCharacter::AddControllerPitchInput);
+    PlayerInputComponent->BindAxis("Turn", this, &AFPSCharacter::AddControllerYawInput);
+
+    PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &AFPSCharacter::BeginCrouch);
+    PlayerInputComponent->BindAction("Crouch", IE_Released, this, &AFPSCharacter::EndCrouch);
+}
+
+void AFPSCharacter::MoveForward(float Value)
+{
+    AddMovementInput(GetActorForwardVector() * Value);
+}
+
+void AFPSCharacter::MoveRight(float Value)
+{
+    AddMovementInput(GetActorRightVector() * Value);
+}
+
+void AFPSCharacter::BeginCrouch()
+{
+    Crouch();
+}
+
+void AFPSCharacter::EndCrouch()
+{
+    UnCrouch();
+}
